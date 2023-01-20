e94a54b4a3c30ada209908bf79eacc8d7e390219
Amos Kohen <amoskohen19@gmail.com>
2023-01-21 10:33:07 +1100

    removed: junk code & fixed: routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Layout } from './core/components/Layout';
 import { RepositoryList } from './pages/Repository/RepositoryList';
 import { RepositoryDetail } from './pages/Repository/RepositoryDetail';
-import { App } from './App';
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path="/" element={<App />} />
           <Route path="/repositories" element={<RepositoryList />} />
           <Route
-            path="/repositories/{repository_id}"
+            path="/repositories/:repositoryName"
             element={<RepositoryDetail />}
           />
+          <Route path="*" element={<Navigate to="/repositories" replace />} />
         </Routes>
       </Layout>
     </BrowserRouter>
