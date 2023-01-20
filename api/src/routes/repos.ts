5d5ef672e99bd2f03efdcd5e30ad61e05de20420
Amos Kohen <amoskohen19@gmail.com>
2023-01-21 10:30:50 +1100

    fixed: backend api

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,39 @@
 import { Router, Request, Response } from 'express';
 
-import { getRepos } from '../services/api';
+import {
+  getCommits,
+  getReadMe,
+  getRepos,
+  getRepository,
+} from '../services/api';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   const allRepos = await getRepos().then((response) => response.data);
-  const repositories = allRepos.filter((repository) => !repository.fork);
+  let repositories = allRepos.filter((repository) => !repository.fork);
+
+  if (_.query.language) {
+    repositories = repositories.filter(
+      (repo) => repo.language === _.query.language
+    );
+  }
 
   return res.status(200).json(repositories);
+});
+
+repos.get('/:name', async (req: Request<{ name: string }>, res: Response) => {
+  const repository = await getRepository(req.params.name).then(
+    (response) => response.data
+  );
+
+  const commits = await getCommits(repository.name).then(
+    (response) => response.data
+  );
+
+  const readMe = await getReadMe(repository.full_name).then(
+    (response) => response.data
+  );
+
+  return res.status(200).json({ ...repository, commits, readMe });
 });
--- api/src/services/api.ts
+++ api/src/services/api.ts
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
 import { Repo } from '../models/Repo';
-import { apiUrl } from '../config/constants';
+import { Commit } from '../models/Commit';
+import { repoListUrl, repoUrl, readMeUrl } from '../config/constants';
 
-const api = axios.create({
-  baseURL: apiUrl,
-});
+const api = axios.create();
 
-export const getRepos = () => api.get<Repo[]>('/repos');
+export const getRepos = () => api.get<Repo[]>(repoListUrl);
+
+export const getRepository = (repoName: string) =>
+  api.get<Repo>(`${repoUrl}/${repoName}`);
+
+export const getCommits = (repoName: string) =>
+  api.get<Commit[]>(`${repoUrl}/${repoName}/commits`);
+
+export const getReadMe = (repoName: string) =>
+  api.get<string>(readMeUrl(repoName));
